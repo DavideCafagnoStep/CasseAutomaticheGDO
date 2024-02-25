efcdf172b2a0af8378134757da3e701cc5c2982f
DavideCafagnoStep <160761468+DavideCafagnoStep@users.noreply.github.com>
2024-02-25 11:55:04 +0100

    Update CASSE AUTOMATICHE QUERY.sql

--- UTILITY CASSE GDO/CASSE AUTOMATICHE QUERY.sql
+++ UTILITY CASSE GDO/CASSE AUTOMATICHE QUERY.sql
@@ -1,7 +1,7 @@
 
 SELECT sum(s.sell_price) as total FROM sells s WHERE s.sell_date = CURRENT_DATE GROUP BY s.sell_date;
 
-SELECT s.sell_pro_id AS pro_id, (SELECT p.pro_description  FROM products p WHERE p.pro_id = s.sell_pro_id) AS product, sum(s.sell_price) as total FROM sells s WHERE s.sell_date = '2024-02-23' GROUP BY s.sell_pro_id;
+SELECT s.sell_pro_id AS pro_id, (SELECT p.pro_description  FROM products p WHERE p.pro_id = s.sell_pro_id) AS product,sum(s.sell_quantity) as quantity, sum(s.sell_price) as total FROM sells s WHERE s.sell_date = '2024-02-23' GROUP BY s.sell_pro_id;
 
 SELECT p.pro_department_id  AS department_id, (SELECT d.department_description AS department FROM departments d WHERE d.department_id = p.pro_department_id),  sum(s.sell_price) as total FROM sells s JOIN products p ON p.pro_id = s.sell_pro_id WHERE s.sell_date = '2024-02-23' GROUP BY p.pro_department_id;
 
